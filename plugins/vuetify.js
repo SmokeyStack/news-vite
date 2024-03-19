cc6becd125458471f6d3e548380229f1b10b9c6b
SmokeyStack <seansyahril04@gmail.com>
2024-03-19 15:53:25 -0400

    Switched to dark mode

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,12 +1,16 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import colors from 'vuetify/util/colors';
 
 export default defineNuxtPlugin((nuxtApp) => {
     const vuetify = createVuetify({
         ssr: true,
         components,
-        directives
+        directives,
+        theme: {
+            defaultTheme: 'dark'
+        }
     });
 
     nuxtApp.vueApp.use(vuetify);
